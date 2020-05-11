4f8e0409c583b4de4dea7ebdfd85fe1bec2ad008
midorikocak <mtkocak@gmail.com>
2020-05-11 18:54:04 +0200

    Pagination

---
+++ resources/js/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <div class="card">
+        <div class="card-header">{{ title }}</div>
+
+        <div class="card-body">
+            {{ content }}
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: ['title'],
+        mounted () {
+            console.log('Component mounted.')
+        },
+    }
+</script>
---
+++ resources/js/components/paper.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: 'paper',
+  }
+</script>
+
+<style scoped>
+
+</style>
